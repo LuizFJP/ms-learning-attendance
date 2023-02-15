a24fd1af3942a593d4908e3d2df79c57f2e25a84
LuizFJP <luizfjportela@gmail.com>
2023-02-15 10:48:56 -0300

    add redis and its config

---
+++ learning-attendance/src/main/java/com/learningattendance/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.learningattendance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+  @Bean
+  JedisConnectionFactory jedisConnectionFactory() {
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+    return new JedisConnectionFactory(config);
+  }
+
+  @Bean
+  RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(jedisConnectionFactory());
+    return template;
+  }
+}
